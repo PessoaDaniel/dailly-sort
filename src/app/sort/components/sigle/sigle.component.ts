1b659f30341198081c18575add69a0d0cdb87121
Daniel Pessoa <daniel@lti.net.br>
2023-04-19 09:25:08 -0300

    sort algorithm

--- src/app/sort/components/sigle/sigle.component.ts
+++ src/app/sort/components/sigle/sigle.component.ts
@@ -1,27 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
 import { NgxSpinnerService } from 'ngx-spinner';
-
+import participants from '../../../json/participans.json';
 
 @Component({
   selector: 'app-sigle',
   templateUrl: './sigle.component.html',
   styleUrls: ['./sigle.component.scss']
 })
-
 export class SigleComponent implements OnInit {
+  participants:Array<any>;
   showCards:boolean;
   sortStarted:boolean;
   sortFinished:boolean;   
   showAll:boolean;
   showSpin:boolean;
 
+
   constructor(private spinner: NgxSpinnerService) { 
     this.showCards = false;
     this.sortStarted = false;
     this.sortFinished = false;
     this.showAll = false;
-    this.showSpin = false
+    this.showSpin = false;
+    this.participants = participants;
   }
 
   ngOnInit(): void {
@@ -29,7 +30,6 @@ export class SigleComponent implements OnInit {
   }
   
   initSort() {
-    // this.showSpin = true;
     this.sortStarted = true;
     this.showCards = true;
   }
@@ -39,6 +39,13 @@ export class SigleComponent implements OnInit {
   }
   slide($event: Event){
     let target = $event.target; 
+  }
+
+  sort() {
+    participants.sort(() => {
+      return - 1
+    });
+    this.initSort();
   }
 
 }
